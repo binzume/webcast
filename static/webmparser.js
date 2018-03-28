84b4eb9d2d0578f1a2637185ac6d7a24883d94a5
Kousuke Kawahira <kawahira@binzume.net>
2018-03-28 12:23:47 +0900

    refactor parser.

--- static/webmparser.js
+++ static/webmparser.js
@@ -26,7 +26,7 @@ class WebmParser {
             0xe7: ['timecode', 'int'],
             0xa3: ['simple_block', 'simple_block'],
             0xa0: ['block_group', 'object'],
-            0xa1: ['block', 'raw'], // TODO
+            0xa1: ['block', 'block'],
             0x1f43b675: ['cluster', '_sibling_'] // ignore cluster in cluster.
         };
 
@@ -61,7 +61,7 @@ class WebmParser {
             0x1a45dfa3: ["ebml", 'object', ebmlSpec],
             0x18538067: ["webm_segment", 'object', segmentSpec]
         };
-        this.currentElement = {name:"_root", type:'object', spec: spec, size: -1, parent: null};
+        this.currentElement = {name:"_root", type:'object', value: {}, spec: spec, size: -1, parent: null};
     }
 
     setListenser(name, cb) {
@@ -87,48 +87,29 @@ class WebmParser {
         return false;
     }
 
-    // return value or null
-    readId() {
+    // return value or null.
+    readVN(clearmsb) {
         if(!this.checkBuf()) return null;
-        let b = this.buffer[this.position];
-        for (let i = 0; i < 8; i++) {
-            if ((b << i) & 0x80) {
-                return this.readNSafe(i + 1);
-            }
-        }
-        return null;
-    }
-
-    // return value or null
-    readInt() {
-        if(!this.checkBuf()) return null;
-        let b = this.buffer[this.position];
-        let m = 0x80;
-        for (let i = 0; i < 8; i++) {
-            if ((b << i) & 0x80) {
-                let v = this.readNSafe(i + 1);
-                if (v !== null) v = v ^ m;
+        let v = this.buffer[this.position], mask = 0x80;
+        for (let i = 0; i < 8; i++, mask >>= 1) {
+            if (v & mask) {
+                if (i + 1 > this.length - this.position) {
+                    return null;
+                }
+                if (clearmsb) v ^= mask;
+                this.position ++;
+                for (; i > 0; i--) {
+                    this.checkBuf();
+                    v = v * 256 + this.buffer[this.position++];
+                }
                 return v;
             }
-            m <<= 7;
         }
         return null;
-    }
-
-    readNSafe(l) {
-        if (l > this.length - this.position) {
-            return null;
-        }
-        let v = 0;
-        for (let i = 0; i < l; i++) {
-            v = (v<<8) | this.buffer[this.position];
-            this.position++;
-            this.checkBuf();
-        }
-        return v;
     }
 
     readBytes(l) {
+        this.checkBuf();
         let p = this.position;
         this.position+=l;
         if (this.position > this.buffer.length) {
@@ -138,39 +119,36 @@ class WebmParser {
         return new Uint8Array(this.buffer.buffer, p, l);
     }
 
-    readByte() {
-        let v = this.buffer[this.position];
-        this.position ++;
-        return v;
-    }
-
     readN(l) {
         let v = 0;
         for (let i = 0; i < l; i++) {
-            v = v * 256 + this.buffer[this.position];
-            this.position ++;
+            this.checkBuf();
+            v = v * 256 + this.buffer[this.position++];
         }
         return v;
     }
 
+    readByte() {
+        this.checkBuf();
+        return this.buffer[this.position++];
+    }
+
     readStr(l) {
-        return String.fromCharCode.apply(null, this.buffer.slice(this.position, this.position+l));
+        return String.fromCharCode.apply(null, this.readBytes(l));
     }
 
     completeElement(e) {
-        // console.log("ok" , e);
         if (this.listener[e.name]) {
             this.listener[e.name](e);
         }
-        if (e.value !== null) {
-            e.parent[e.name] = e.value;
-        }
+        e.parent.value[e.name] = e.value;
+        // console.log("ok" , e);
         return e.parent;
     }
 
     tryParse() {
         let objectCountLimit = 1000;
-        for (var t = 0 ;t < objectCountLimit; t ++) {
+        for (let t = 0 ;t < objectCountLimit; t ++) {
             let e = this.currentElement;
             while (e.size >= 0 && (e.start - this.offset + e.size == this.position)) {
                 e = this.completeElement(e);
@@ -178,12 +156,12 @@ class WebmParser {
             }
 
             if (this.currentElement.type == 'object') {
-                let childId = this.readId();
+                let childId = this.readVN(false);
                 if (childId === null) return;
                 this.currentElement = {id: childId, spec: {}, value: null, size: null, parent: this.currentElement};
             }
             if (this.currentElement.size === null) {
-                let size = this.readInt();
+                let size = this.readVN(true);
                 if (size === null) return;
                 this.currentElement.size = size;
                 this.currentElement.start = this.position + this.offset;
@@ -198,17 +176,17 @@ class WebmParser {
                 this.currentElement.name = type[0];
                 this.currentElement.type = type[1];
                 if (type[1] == 'object') {
+                    this.currentElement.value = {};
                     this.currentElement.spec = type[2] || parent.spec;
                     continue;
                 }
             }
-
             if (this.currentElement.size < 0 || this.length - this.position < this.currentElement.size) {
                 return;
             }
 
-            if (this.currentElement.type =='simple_block') {
-                let tr = this.readInt(), t = this.readN(2), f = this.readByte();
+            if (this.currentElement.type =='simple_block' || this.currentElement.type == 'block') {
+                let tr = this.readVN(true), t = this.readN(2), f = this.readByte();
                 let sz =  this.currentElement.start + this.currentElement.size - this.position - this.offset;
                 this.currentElement.value = {track: tr, timecode: t, flags: f, payload: this.readBytes(sz)};
             } else if (this.currentElement.type =='int') {
